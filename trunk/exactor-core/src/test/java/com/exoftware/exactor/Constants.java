a0cf1c7e03bfcc35aa0f3d949846a312be84484b
michaellieshoff@gmx.de <michaellieshoff@gmx.de@ee98d24a-40ea-6f78-f106-5fcd743a176e>
2013-01-30 11:09:08 +0000

    Issue 17: splitted subprojects.

--- trunk/exactor-core/src/test/java/com/exoftware/exactor/Constants.java
+++ trunk/exactor-core/src/test/java/com/exoftware/exactor/Constants.java
@@ -8,15 +8,13 @@ import java.io.File;
  *
  * @author Brian Swan
  */
-public class Constants
-{
+public class Constants {
     public static final String FS = File.separator;
-    public static final String TEST_DIR = System.getProperty( "user.dir" ) + FS + "src" + FS + "test" + FS;
-    public static final String DATA_DIR = TEST_DIR + "resources/data" + FS;
-    public static final String NEW_LINE = System.getProperty( "line.separator" );
+    public static final String TEST_DIR = System.getProperty("user.dir") + "/src/test/";
+    public static final String DATA_DIR = TEST_DIR + "resources/data/";
+    public static final String NEW_LINE = System.getProperty("line.separator");
 
-    public static String pathToTestFile(String testfile)
-    {
+    public static String pathToTestFile(String testfile) {
        return Constants.DATA_DIR + testfile;
-    }    
+    }
 }
